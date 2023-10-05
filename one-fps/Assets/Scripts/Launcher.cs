46b218161eb6c8c90f6a4d83c238d879c6e6215c
Hakkı Tumar <77984587+lanco0@users.noreply.github.com>
2023-10-05 15:29:36 +0300

    Update

--- one-fps/Assets/Scripts/Launcher.cs
+++ one-fps/Assets/Scripts/Launcher.cs
@@ -6,7 +6,10 @@ using TMPro;
 
 public class Launcher : MonoBehaviourPunCallbacks
 {
-    
+    [SerializeField] TMP_InputField roomNameInputField;
+    [SerializeField] TMP_Text errorText;
+    [SerializeField] TMP_Text roomnNameText;
+
     void Start()
     {
         Debug.Log("Connecting");
@@ -27,7 +30,36 @@ public class Launcher : MonoBehaviourPunCallbacks
 
     public void CreateRoom()
     {
+        if (string.IsNullOrEmpty(roomNameInputField.text))
+        {
+            return;
+        }
 
+        PhotonNetwork.CreateRoom(roomNameInputField.text);
+        MenuManager.instance.OpenMenu("loading");
     }
-   
+
+    public override void OnJoinedRoom()
+    {
+        MenuManager.instance.OpenMenu("room");
+        roomnNameText.text = PhotonNetwork.CurrentRoom.Name;
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        errorText.text = "Room Creation Failed:" + message;
+        MenuManager.instance.OpenMenu("error");
+    }
+
+    public void leaveRoom()
+    {
+        PhotonNetwork.LeaveRoom();
+        MenuManager.instance.OpenMenu("loading");
+    }
+
+    public override void OnLeftRoom()
+    {
+        MenuManager.instance.OpenMenu("title");
+    }
+
 }
