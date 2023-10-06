7c3281664fc19fa29679ccd77b2c4c7d634629b2
Hakkı Tumar <77984587+lanco0@users.noreply.github.com>
2023-10-07 00:58:42 +0300

    update

---
+++ one-fps/Assets/Scripts/Ithem.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ithem : MonoBehaviour
+{
+   public IthemInfo Info;
+   public GameObject IthemGameObject;
+}
---
+++ one-fps/Assets/Scripts/IthemInfo.cs
@@ -0,0 +1,8 @@
+ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IthemInfo : ScriptableObject
+{
+    public string ithemName;
+}
